d4c45104b6d2bb736988bb214659f21c974acd0b
Sergio Guachalla <sergioguachalla0@gmail.com>
2024-04-13 15:04:01 -0400

    add feign dependency

--- src/main/kotlin/com/users/msusers/MsUsersApplication.kt
+++ src/main/kotlin/com/users/msusers/MsUsersApplication.kt
@@ -3,6 +3,7 @@ package com.users.msusers
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
 @EnableDiscoveryClient
